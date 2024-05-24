ee6285b2b0f5f2791cbb01725c0ee5a5a0717c97
Siddharth Singh Chouhan <62793364+sidchouhan27@users.noreply.github.com>
2024-05-24 15:01:06 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Data Engineer.", "Research Assistant.", "Student."],
+		strings: ["Eat", "Sleep", "Code();", "Repeat],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
